af971e23f44b69c2e1af781d671ddef13c32823d
Mark Kaye <mark@kaye.co>
2020-12-23 19:15:41 +0000

    Added boot script and altered config

---
+++ boot_ansible.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#######################################################################
+# Script Name: boot_ansible.sh
+# Description: This script installs my Mac development machine
+#     Options: None
+#
+#      Author: Mark Kaye
+#       Email: mark@kaye.co
+#     Version: 1.0
+#        Date: 20201223 19:07 GMT
+#######################################################################
+
+# Start by installing Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Use Homebrew to install Ansible
+brew install ansible
+
+# Copy my SSH keypair
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cp /Volumes/Install\ macOS\ Big\ Sur/id_rsa* ~/.ssh
+chmod 600 ~/.ssh/id_rsa*
+
+# Create some basic directories to get stuff done
+mkdir -p ~/Development/ansible
+
+# Clone the Mac setup repo
+cd ~/Development/ansible
+git clone git@github.com:mpkayeuk/mac-dev-playbook.git
+
+# Download the playbook requirements
+cd ~/Development/ansible/mac-dev-playbook
+ansible-galaxy install -r requirements.yml
+
+# Let's go!
+echo "Ready to rock and roll!  Run the playbook."
